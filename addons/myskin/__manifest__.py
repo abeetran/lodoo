48b854c2ec28896a6525d7bc11cdcae5eadcff5e
Abeetran <abeetran@gmail.com>
2026-06-05 18:43:13 +0700

    update template for my skin

--- addons/myskin/__manifest__.py
+++ addons/myskin/__manifest__.py
@@ -8,15 +8,14 @@
         "web",
     ],
     "data": [
-        # "views/assets.xml",
-        "views/apps.xml",
+        "views/assets.xml",
     ],
     "assets": {
         "web.assets_backend": [
             "myskin/static/src/css/backend.css",
-            "myskin/static/src/js/backend.js",
-            "myskin/static/src/js/hide_odoo_account.js",
-            "myskin/static/src/js/default_home.js",
+            "myskin/static/src/js/*.js",
+            # "myskin/static/src/js/hide_odoo_account.js",
+            # "myskin/static/src/js/default_home.js",
         ],
         "web.assets_frontend": [
             "myskin/static/src/css/login.css",
